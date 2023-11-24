fe3dbfb016213e795cf80dab837f0f7cb0a653f3
kiru729 <48012773+kiru729@users.noreply.github.com>
2023-11-24 17:36:58 +0530

    Update StartApplication.java

--- src/main/java/com/devops/coach/StartApplication.java
+++ src/main/java/com/devops/coach/StartApplication.java
@@ -12,8 +12,8 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "Howdy Folks from Coach AK Training..!!!! ");
-        model.addAttribute("msg", "Hello All..We are deploying springboot application into EKS cluster using Helm + Jenkins Pipeline!!!!");
+        model.addAttribute("title", "Hello folks from Kiru..!!!! ");
+        model.addAttribute("msg", "Hello All..We are deploying springboot application into EKS cluster using Helm + Jenkins Pipeline and it is awesome!!!!");
         return "index";
     }
 
